62cce40dee8d8f5456d3f5ef0502d7d518aa27b5
ardita1 <ardiitamorina@gmail.com>
2018-08-28 14:45:23 +0200

    Rename View Controller

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Ardita on 8/28/18.
+//  Copyright © 2018 Ardita. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
